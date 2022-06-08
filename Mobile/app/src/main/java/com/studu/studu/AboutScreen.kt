43c1761f938877dcbc207978684c6428ee870df7
Raden Rofiq Yudha Setyawan <99266876+rofiqyudha@users.noreply.github.com>
2022-06-08 15:19:44 +0700

    Add files via upload

---
+++ Mobile/app/src/main/java/com/studu/studu/AboutScreen.kt
@@ -0,0 +1,11 @@
+package com.studu.studu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_screen)
+    }
+}
---
+++ Mobile/app/src/main/java/com/studu/studu/EditProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.studu.studu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile_screen)
+    }
+}
